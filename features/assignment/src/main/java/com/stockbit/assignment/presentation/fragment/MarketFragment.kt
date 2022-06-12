98654589b6a317ebd5cecb05eeb4812ff176446a
Rendra Prasetia <rendra.prasetia@bankmandiri.co.id>
2022-06-12 10:36:12 +0700

    adding logic in MarketFragment.kt and MarketViewModel.kt to showing data from api

--- features/assignment/src/main/java/com/stockbit/assignment/presentation/fragment/MarketFragment.kt
+++ features/assignment/src/main/java/com/stockbit/assignment/presentation/fragment/MarketFragment.kt
@@ -1,15 +1,41 @@
 package com.stockbit.assignment.presentation.fragment
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.stockbit.assignment.R
 import com.stockbit.assignment.databinding.FragmentMarketBinding
+import com.stockbit.assignment.presentation.adapter.MarketAdapter
 import com.stockbit.assignment.presentation.viewmodel.MarketViewModel
+import com.stockbit.assignment.utils.Constants.BASE_URL
 import com.stockbit.common.base.BaseFragment
+import com.stockbit.common.extension.hide
+import com.stockbit.common.extension.orFalse
+import com.stockbit.common.extension.show
+import com.stockbit.common.utils.ErrorState
+import com.stockbit.model.Result
+import com.stockbit.model.dto.response.TopListVolumeFullDataResponse
 import org.koin.android.viewmodel.ext.android.viewModel
 
-class MarketFragment : BaseFragment<FragmentMarketBinding>() {
+class MarketFragment : BaseFragment<FragmentMarketBinding>(), SwipeRefreshLayout.OnRefreshListener {
 
     private val viewModel by viewModel<MarketViewModel>()
+
+    private val marketAdapter by lazy {
+        MarketAdapter {
+            activity?.startActivity(
+                Intent(Intent.ACTION_VIEW).apply {
+                    data = Uri.parse(BASE_URL + it.url)
+                }
+            )
+        }
+    }
 
     override fun initBinding(
         inflater: LayoutInflater,
@@ -17,7 +43,107 @@ class MarketFragment : BaseFragment<FragmentMarketBinding>() {
     ): FragmentMarketBinding = FragmentMarketBinding.inflate(inflater, container, false)
 
     override fun initView() {
+        observeNavigation(viewModel)
+        initUI()
+        viewModel.resetValue()
+        viewModel.getTotalVolFull()
+        setupObservers()
+    }
 
+    private fun initUI() = with(binding) {
+        rvMarket.apply {
+            adapter = marketAdapter
+            layoutManager = LinearLayoutManager(context)
+            addItemDecoration(DividerItemDecoration(context, RecyclerView.VERTICAL))
+        }
+        swipeContainer.setOnRefreshListener(this@MarketFragment)
+        errorView.retryListener = { onRefresh() }
+        scrollContainer.setOnScrollChangeListener(
+            NestedScrollView.OnScrollChangeListener { v, _, scrollY, _, _ ->
+                if (scrollY == (v.getChildAt(0).measuredHeight - v.measuredHeight) && viewModel.isPreviousDataLoaded)
+                    viewModel.getTotalVolFull(isNextPage = true)
+            }
+        )
+    }
+
+    private fun setupObservers() {
+        observeTopTotalList()
+        observeTicker()
+    }
+
+    private fun observeTopTotalList() = with(viewModel) {
+        topTotalVolFull.observe(viewLifecycleOwner) {
+            isPreviousDataLoaded = it.status == Result.Status.SUCCESS
+            when (it.status) {
+                Result.Status.SUCCESS -> onTopTotalSuccess(it.data)
+                Result.Status.LOADING -> onTopTotalLoading()
+                Result.Status.ERROR -> onTopTotalError(it)
+            }
+        }
+    }
+
+    private fun observeTicker() = with(viewModel) {
+//        observeSocketData.observe(viewLifecycleOwner) {
+//            if (it.openDay > 0) marketAdapter.openDay[it.fromSymbol] = it.openDay
+//            marketAdapter.updatePrice(it)
+//        }
+    }
+
+    private fun onTopTotalSuccess(data: TopListVolumeFullDataResponse?) = with(binding) {
+//        viewModel.connectToWebSocket(data?.data)
+        if (viewModel.isLoadNextPage) {
+            tvLoadMore.hide()
+            marketAdapter.submitList(marketAdapter.currentList.plus(data?.data.orEmpty()))
+        } else {
+            hideShimmer()
+            if (data?.data?.isNotEmpty().orFalse()) {
+                rvMarket.show()
+                marketAdapter.submitList(data?.data)
+                errorView.hide()
+            } else {
+                rvMarket.hide()
+                errorView.apply {
+                    show()
+                    setError(ErrorState.EMPTY, getString(R.string.error_empty))
+                }
+            }
+        }
+    }
+
+    private fun onTopTotalLoading() = with(binding) {
+        if (marketAdapter.currentList.isEmpty() && !viewModel.isLoadNextPage) {
+            showShimmer()
+            rvMarket.hide()
+            errorView.hide()
+        } else tvLoadMore.show()
+    }
+
+    private fun onTopTotalError(result: Result<TopListVolumeFullDataResponse>) = with(binding) {
+        hideShimmer()
+        tvLoadMore.hide()
+        errorView.apply {
+            show()
+            setError(ErrorState.ERROR, result.message.orEmpty())
+        }
+    }
+
+    private fun showShimmer() = with(binding) {
+        rvShimmer.show()
+        rvShimmer.showShimmerAdapter()
+    }
+
+    private fun hideShimmer() = with(binding) {
+        rvShimmer.hide()
+        rvShimmer.hideShimmerAdapter()
+    }
+
+    override fun onRefresh() = with(binding) {
+        swipeContainer.isRefreshing = false
+        marketAdapter.submitList(null)
+        showShimmer()
+        rvMarket.hide()
+        viewModel.resetValue()
+        viewModel.getTotalVolFull()
     }
 
 }
--- features/assignment/src/main/java/com/stockbit/assignment/presentation/viewmodel/MarketViewModel.kt
+++ features/assignment/src/main/java/com/stockbit/assignment/presentation/viewmodel/MarketViewModel.kt
@@ -1,7 +1,68 @@
 package com.stockbit.assignment.presentation.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.asLiveData
+import com.sandbox.domain.domain.ConnectToWebSocketUseCase
+import com.sandbox.domain.domain.GetTopTotalVolumeFullUseCase
+import com.sandbox.domain.domain.ReceiveResponseUseCase
 import com.stockbit.common.base.BaseViewModel
+import com.stockbit.model.Result
+import com.stockbit.model.dto.request.SubscriptionRequest
+import com.stockbit.model.dto.response.TopListData
+import com.stockbit.model.dto.response.TopListVolumeFullDataResponse
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.flowOn
 
-class MarketViewModel : BaseViewModel() {
+class MarketViewModel(
+    private val getTopTotalVolumeFullUseCase: GetTopTotalVolumeFullUseCase,
+    private val connectToWebSocketUseCase: ConnectToWebSocketUseCase,
+    private val receiveResponseUseCase: ReceiveResponseUseCase
+) : BaseViewModel() {
+
+    private val _topTotalVolFull = MutableLiveData<Result<TopListVolumeFullDataResponse>>()
+    val topTotalVolFull: LiveData<Result<TopListVolumeFullDataResponse>> = _topTotalVolFull
+
+    var currency = "IDR"
+    var currentPage = 1
+    var isPreviousDataLoaded = false
+    var isLoadNextPage = false
+
+    val observeSocketData by lazy {
+        receiveResponseUseCase().flowOn(Dispatchers.IO).filter { it.type == 5 }.asLiveData()
+    }
+
+    fun resetValue() {
+        currentPage = 1
+    }
+
+    fun getTotalVolFull(isNextPage: Boolean = false) {
+        if (isNextPage) currentPage++
+        isLoadNextPage = isNextPage
+
+        launchWithViewModel {
+            getTopTotalVolumeFullUseCase(
+                page = currentPage,
+                currency = currency
+            ).collect { _topTotalVolFull.postValue(it) }
+        }
+    }
+
+    fun connectToWebSocket(data: List<TopListData>?) {
+        val subs = mutableListOf<String>()
+        data?.let {
+            it.forEach { topList ->
+                subs.add("5~CCCAGG~${topList.coinInfo.name}~$currency")
+            }
+        }
+        connectToWebSocketUseCase(
+            SubscriptionRequest(
+                action = "SubAdd",
+                subs = subs
+            )
+        )
+    }
 
 }
